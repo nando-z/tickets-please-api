f0d3e0b5dc30a1769742034058ff47e32291c100
nando-z <157473087+nando-z@users.noreply.github.com>
2025-04-01 21:41:34 +0200

    Refactor AuthController methods for improved readability and consistency

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,18 +2,16 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
 use App\Http\Requests\ApiLoginRequest as LoginRequest;
 use App\Http\Requests\ApiRegisterRequest as RegisterRequest;
 use App\Models\User;
-
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
     use \App\Traits\ApiResponses;
-    // Register a new user
+
     public function register(RegisterRequest $request)
     {
         $user = User::create(
@@ -23,57 +21,34 @@ class AuthController extends Controller
             ]
         );
 
-        // Create a token for the user after creation
         $token = $user->createToken('API TOKEN FOR ' . $user->email)->plainTextToken;
 
-        return $this->success(
-            'User created successfully',
-            [
-                'user' => $user,
-                'token' => $token,
-            ],
-
-        );
+        return $this->success('User created successfully', [
+            'user' => $user,
+            'token' => $token,
+        ]);
     }
 
-
-    // Login a user
     public function login(LoginRequest $request)
     {
-        // Validate the request
-        $request->validated(
-            $request->all()
-        );
-        // and return it in the response
         if (!Auth::attempt($request->only('email', 'password'))) {
-
-            return $this->error(
-                'Invalid credentials',
-                [
-                    'password' => ['The provided credentials are incorrect.'],
-                    'email' => ['The provided credentials are incorrect.'],
-                ],
-                401
-            );
+            return $this->error('Invalid credentials', [
+                'email' => ['The provided credentials are incorrect.'],
+                'password' => ['The provided credentials are incorrect.'],
+            ], 401);
         }
-        // If the user is found and the password is correct, create a new token for the user
-        // and return it in the response
-        $user = User::firstWhere('email', $request->get('email'));
 
+        $user = Auth::user();
+        $token = $user->createToken('API TOKEN FOR ' . $user->email)->plainTextToken;
 
-
-        return $this->ok(
-            'Authenticated',
-            [
-                'user' => $user,
-                'token' => $user->createToken('API TOKEN FOR ' . $user->email)->plainTextToken,
-            ]
-        );
+        return $this->ok([
+            'user' => $user,
+            'token' => $token,
+        ], 'Login authenticated. Welcome back, ' . $user->name);
     }
 
     public function logout(Request $request)
     {
-        // Revoke the token that was used to authenticate the request
         $request->user()->currentAccessToken()->delete();
 
         return $this->ok('Logged out successfully');
