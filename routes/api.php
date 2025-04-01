e6185ec8731ba4e3e37b5b0c1e2ef7bf0866088c
nando-z <157473087+nando-z@users.noreply.github.com>
2025-04-01 13:48:16 +0200

    Add environment configuration file and clean up routes

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,7 @@
 <?php
 
-
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
-
 
 Route::post('/login', [\App\Http\Controllers\AuthController::class, 'login']);
 Route::post('/register', [\App\Http\Controllers\AuthController::class, 'register']);
