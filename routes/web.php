a143a04a15a99b87a2a4722c46986adb7209308b
nando-z <157473087+nando-z@users.noreply.github.com>
2025-04-03 18:47:06 +0200

    Update API root response to include full URLs for login, registration, and tickets routes

--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,15 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return response()->json([
-	"message" =>"Api Runing Will",
-	"status" => 200,
-	"routs"=>[
-		"/api/login",
-		"/api/register",
-		"api/v1/tickets",
-	]
-],200);
+	return response()->json([
+
+		"message" => "Api Runing Will",
+		"status" => 200,
+		"routs" => [
+			"/api/login" => "https:127.0.0.1:8000/api/login",
+			"/api/register" => "https:127.0.0.1:8000/api/v1/register",
+			"api/v1/tickets" => "https:127.0.0.1:8000/api/v1/tickets",
+
+		]
+	]);
 });
